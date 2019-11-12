677ff868f4893cc612b6a26f1407cf7bed8af2ab
Dyphare <erwanferry79@gmail.com>
2019-11-12 16:57:37 +0100

    activite 0.0.3

--- resources/views/pages/activites.blade.php
+++ resources/views/pages/activites.blade.php
@@ -4,8 +4,22 @@
 
 <div class = 'cadre'>
     <h1>Activités</h1>
-  <form action="" method="get">
-  
+  <form  method="get">
+       <p>
+            <label for="nom"></label>
+            
+
+            <label for="description">Description</label><br>
+           
+
+            <label for="frequence">Fréquence de la manifestation</label><br>
+          
+
+            <label for="image">Image</label><br>
+            
+
+            
+        </p>
   </form>
 </div>
 @endsection
