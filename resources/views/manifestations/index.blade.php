388b2e6aca2f853766a244a811b84d6315c789a1
Dyphare <erwanferry79@gmail.com>
2019-11-13 16:29:34 +0100

    activité 0.0.5

--- resources/views/manifestations/index.blade.php
+++ resources/views/manifestations/index.blade.php
@@ -6,7 +6,15 @@
     @if (count($manifestations) > 0)
         @foreach ($manifestations as $manifestation)
             <h3><a href="/manifestations/{{$manifestation->id}}">{{$manifestation->nom}}</a></h3>
+            <h3>{{$manifestation->description}}</h3>
+            <h3>{{$manifestation->image}}</h3>
+            <h3>{{$manifestation->frequence}}</h3>
+            <h3>{{$manifestation->date}}</h3>
+            <h3>{{$manifestation->prix}}</h3>
+            <textarea name="commentaire" id="" cols="30" rows="10"></textarea>
+            <button>like</button>
             <small>Créer à {{$manifestation->created_at}}</small>
+
         @endforeach
     @else
         <p>Pas d'activité d'organisées</p>
