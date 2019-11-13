bb2618457a656cd0e4144d5df84678ba40cea679
Gatata <sacha.garnier@live.fr>
2019-11-13 13:50:20 +0100

    Manifestation 0.3.1

--- resources/views/manifestations/index.blade.php
+++ resources/views/manifestations/index.blade.php
@@ -5,7 +5,8 @@
     <h1>Manifestations</h1>
     @if (count($manifestations) > 0)
         @foreach ($manifestations as $manifestation)
-            <h3>{{$manifestation->nom}}</a></h3>
+            <h3><a href="/manifestations/{{$manifestation->id}}">{{$manifestation->nom}}</a></h3>
+            <small>Créer à {{$manifestation->created_at}}</small>
         @endforeach
     @else
         <p>Pas d'activité d'organisées</p>
