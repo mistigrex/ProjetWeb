e29041b60ad46225a496c44fb44fb82f07473283
Dyphare <erwanferry79@gmail.com>
2019-11-14 10:14:42 +0100

    activité 1.2

--- resources/views/manifestations/show.blade.php
+++ resources/views/manifestations/show.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
 <div class = cadre>
-   <a href="/manifestations" class= "btn btn-default">retour</a>
+   <a href="/manifestations/{{$manifestation->id}}" class= "btn btn-default">retour</a>
         <h1>{{$manifestation->nom}}</h1>
         <div>
-            {{$manifestation->description}}
-            {{$manifestation->image}}
-            {{$manifestation->frequence}}
-            {{$manifestation->date}}
-            {{$manifestation->prix}}
+            {{$manifestation->description}} <br>
+            {{$manifestation->image}}<br>
+            {{$manifestation->frequence}}<br>
+            {{$manifestation->date}}<br>
+            {{$manifestation->prix}}<br>
             <textarea name="commentaire" id="" cols="30" rows="10"></textarea>
             <button><img class = 'like' src="{{asset('images/like.png')}}"alt="like"></button>
-        
+        // recup id utilisateur et id activité
         </div>
            
             
