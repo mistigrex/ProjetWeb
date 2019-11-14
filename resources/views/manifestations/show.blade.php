08040a36062db4c40450b8cc2fa65914daf34902
Dyphare <erwanferry79@gmail.com>
2019-11-14 09:01:18 +0100

    activité 1.0

--- resources/views/manifestations/show.blade.php
+++ resources/views/manifestations/show.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 <div class = cadre>
-    <h1>Manifestations</h1>
-    @if (count($manifestations) > 0)
-        @foreach ($manifestations as $manifestation)
-            <h3><a href="/manifestations/{{$manifestation->id}}">{{$manifestation->nom}}</a></h3>
+   <a href="/manifestations" class= "btn btn-default">retour</a>
+        <h1>{{$manifestation->nom}}</h1>
+        <div>
             {{$manifestation->description}}
             {{$manifestation->image}}
             {{$manifestation->frequence}}
@@ -13,12 +12,10 @@
             {{$manifestation->prix}}
             <textarea name="commentaire" id="" cols="30" rows="10"></textarea>
             <button>like</button>
-            <small>Créer à {{$manifestation->created_at}}</small>
-
-        @endforeach
-    @else
-        <p>Pas d'activité d'organisées</p>
-    @endif
+        
+        </div>
+           
+            
 </div>
 
 
