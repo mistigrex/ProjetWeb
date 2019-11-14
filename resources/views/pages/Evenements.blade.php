310d948fd513f78de6f290150409c8b235d646e1
Gatata <sacha.garnier@live.fr>
2019-11-14 10:03:47 +0100

    Manifestation 1.0.2

--- resources/views/pages/Evenements.blade.php
+++ resources/views/pages/Evenements.blade.php
@@ -3,9 +3,18 @@
 @section('content')
 <div class = "cadre">
     @auth
-    if(Auth::user()-role_id == 0)
-    <a href="/manifestations/create"><input type="button" value="Ta mère"></a>
+        @if (Auth::user()->Role_id == 0)
+            <a href="/manifestations/create">
+                <img src="https://i.skyrock.net/4864/63484864/pics/2558063637_small_1.jpg" alt="la description textuelle de ton image" />
+            </a>
+        @endif
+
     @endauth
+
+
+    <!---if(Auth::user()-Role_id == 0){
+    <a href="/manifestations/create"><input type="btn btn-primary" value="Créer une manifestation"></a>--->
+
 </div>
 
 @endsection
