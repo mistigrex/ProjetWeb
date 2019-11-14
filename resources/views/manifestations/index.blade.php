d617d123783def7aa9e5eeb1381ceb1ec84dcb72
Dyphare <erwanferry79@gmail.com>
2019-11-14 13:54:36 +0100

    activity 1.5 manque juste commentaire

--- resources/views/manifestations/index.blade.php
+++ resources/views/manifestations/index.blade.php
@@ -8,11 +8,12 @@
             <h3><a href="/manifestations/{{$manifestation->id}}">{{$manifestation->nom}}</a></h3>
          
             <small>Créer à {{$manifestation->created_at}}</small>
-
+        
         @endforeach
     @else
         <p>Pas d'activité d'organisées</p>
     @endif
+  
 </div>
 
 
