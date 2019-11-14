af1a0cd8fbefe0706b76169e1545813e846d11d5
Dyphare <erwanferry79@gmail.com>
2019-11-14 11:13:17 +0100

    activité 1.3

--- resources/views/manifestations/show.blade.php
+++ resources/views/manifestations/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class = cadre>
-   <a href="/manifestations/{{$manifestation->id}}" class= "btn btn-default">retour</a>
+   <!--<a href="/manifestations/{{$manifestation->id}}" class= "btn btn-default">retour</a>-->
         <h1>{{$manifestation->nom}}</h1>
         <div>
             {{$manifestation->description}} <br>
@@ -11,7 +11,14 @@
             {{$manifestation->date}}<br>
             {{$manifestation->prix}}<br>
             <textarea name="commentaire" id="" cols="30" rows="10"></textarea>
+            <button>envoyer commentaire</button>
+            @if (count ({{$manisfestation->commentaire}}) > 0)
+                {{$manifestation->commentaire}}<br>
+            @else  
+                <p>Page encore de commentaire, sois le premier à commenter</p>
+            @endif
             <button><img class = 'like' src="{{asset('images/like.png')}}"alt="like"></button>
+            <button>S'incrire à l'événement</button>
         // recup id utilisateur et id activité
         </div>
            
