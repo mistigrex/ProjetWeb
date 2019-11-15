2a64257e15e0997fc17b1b4033ca2c3b1a6f1fce
ssweal <swealllllll@gmail.com>
2019-11-15 19:08:32 +0100

    Sécurisation du site

--- app/Http/Controllers/AdministrationsController.php
+++ app/Http/Controllers/AdministrationsController.php
@@ -8,6 +8,19 @@ use App\User;
 
 class AdministrationsController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+
+        
+    }
+
     /**
      * Display a listing of the resource.
      *
